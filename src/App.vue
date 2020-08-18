020de288581a877db6511b9dfc4b500681da5e0c
conantroutman <johsod96@gmail.com>
2020-08-18 02:14:26 +0200

    Sort of working code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,95 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <label for="raceSelect">Select Race:</label>
+    <select name="raceSelect" id="raceSelect" v-model="race">
+      <option value="argonian">Argonian</option>
+      <option value="breton">Breton</option>
+      <option value="dunmer">Dark Elf</option>
+      <option value="altmer">High Elf</option>
+      <option value="imperial">Imperial</option>
+      <option value="khajiit">Khajiit</option>
+      <option value="nord">Nord</option>
+      <option value="orc">Orc</option>
+      <option value="redguard">Redguard</option>
+      <option value="bosmer">Wood Elf</option>
+    </select>
+    <select name="gender" id="genderSelect" v-model="gender">
+      <option value="male">Male</option>
+      <option value="female">Female</option>
+    </select>
+    <span v-if="hasSurname">
+      <label for="toggleSurname">Generate Family Name:</label>
+      <input type="checkbox" id="toggleSurname" v-model="toggleSurname">
+    </span>
+    <button v-on:click="generateName()">Generate</button>
+    <h2 id="name">{{ fullName }}</h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data: () => {
+    return {
+      fullName: '',
+      race: 'argonian',
+      gender: 'male',
+      isReady: false,
+      toggleSurname: true,
+      hasSurname: true
+    }
+  },
+  methods: {
+    generateName() {
+      const names = require(`./names/${this.race}.json`);
+      // Hide toggle family name button if the selected race does not have family names
+      this.toggleSurname = names.hasSurname;
+      const name = this.getRandomName(this.gender == 'male' ? names.male : names.female);
+      let surname = '';
+      if(this.toggleSurname) {
+        switch(this.race) {
+          // Orc family name is based on the parent's name & gender based
+          case 'orc':
+            surname = this.getOrcSurname(names);
+            break;
+          // Redguard family names can be based on relatives or birthplace
+          case 'redguard':
+            surname = this.getRedguardSurname(names);
+            break;
+          default:
+            surname = this.getRandomName(names.surnames)
+            break;
+        }
+      }
+      this.fullName = `${name} ${surname}`;
+    },
+
+    getRandomName(array) {
+      const random = Math.floor(Math.random() * array.length);
+      return array[random];
+    },
+
+    getOrcSurname(names) {
+      const prefix = (this.gender == 'male' ? "gro-" : "gra-");
+      return `${prefix}${this.getRandomName(this.gender == 'male' ? names.male : names.female)}`;
+    },
+
+    getRedguardSurname(names) {
+      const prefixes = ["at", "af", "al"];
+      const random = Math.floor(Math.random() * prefixes.length);
+      // 2 equals name based on birthplace
+      const suffix = this.getRandomName(random === 2 ? names.birthplaces : names.male.concat(names.female));
+      return `${prefixes[random]}-${suffix}`;
+    }
+  },
+  computed: {
+
+  },
+  watch: {
+
   }
 }
 </script>
